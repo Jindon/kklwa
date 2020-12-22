e80370c8c622a86054c7b7aee41efba72b199adb
Jindon Moirangthem <jindon27@gmail.com>
2020-12-22 23:21:27 +0530

    update logo component

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -2,10 +2,10 @@
 
 <div class="flex items-center space-x-0 md:space-x-3">
     {{-- <span class="p-4 text-center text-green-700 bg-green-200 rounded">Logo</span> --}}
-    <div class="w-auto h-16 overflow-auto rounded">
-        <img src="{{ $logoUrl }}" alt="Staff photo" class="w-auto h-16">
+    <div class="w-auto h-12 overflow-auto rounded">
+        <img src="{{ $logoUrl }}" alt="Staff photo" class="w-auto h-12">
     </div>
     @unless($onlyLogo)
-        <div class="hidden w-40 text-sm font-black leading-4 text-green-700 uppercase md:block">{{ $sitename }}</div>
+        <div class="hidden w-56 text-sm font-black leading-4 text-green-700 uppercase md:block">{{ $sitename }}</div>
     @endunless
 </div>
