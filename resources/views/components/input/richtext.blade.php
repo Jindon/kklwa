fe79df45057b9558e390c475e390fface8bfa2eb
Jindon Moirangthem <jindon27@gmail.com>
2020-12-25 13:14:06 +0530

    disable textarea file upload

--- resources/views/components/input/richtext.blade.php
+++ resources/views/components/input/richtext.blade.php
@@ -6,6 +6,11 @@
     {{ $attributes }}
     x-data
     @trix-blur="$dispatch('change', $event.target.value)"
+    x-init="
+        this.addEventListener('trix-file-accept', e => {
+            e.preventDefault();
+        });
+    "
 >
     <input id="x" value="{{ $initialValue }}" type="hidden">
     <trix-editor class="trix-content" input="x" class="border border-gray-300 rounded"></trix-editor>
